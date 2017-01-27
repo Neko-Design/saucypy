07dd8b264b49b7f644b47f9ff6eda97c1532900d
Ewen McCahon <ewen.mccahon@gmail.com>
2017-01-27 19:50:59 +1100

    Modified and Packaged Saucyy

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+    name='saucypy',
+    packages=['saucypy'],
+    install_requires=[
+        'json',
+        'requests'
+    ],
+    version='0.3',
+    description='SauceLabs API Python Wrapper',
+    author='Ewen McCahon',
+    author_email='ewen.m.mccahon@student.uts.edu.au',
+    url='https://github.com/Neko-Design/saucypy',
+    download_url='https://github.com/Neko-Design/saucypy/tarball/0.3',
+    keywords=['saucelabs', 'api', 'wrapper', 'interface', 'sauce'],
+    classifiers=[]
+)
